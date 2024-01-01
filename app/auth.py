f1d7d2d892915aa0883a7c2691d6f99a99b98e96
David Jahyer <david.jahyer@gmail.com>
2024-01-01 16:23:59 +0100

    Introduce error handling decorator, refactor functions for centralized error handling.

--- app/auth.py
+++ app/auth.py
@@ -1,31 +1,25 @@
 from dotenv import load_dotenv
 import os, spotipy
-from app.utils import printc
+from app.utils import printc, error_handler
 
 load_dotenv()
 
+@error_handler
 def validate_environment_variables(required_vars):
     for var in required_vars:
         if var not in os.environ:
-            printc(f"Missing environment variable: {var}", "red")
             raise ValueError(f"Missing environment variable: {var}")
 
+@error_handler
 def connect_to_spotify():
-    try:
-        sp = spotipy.Spotify(
-            auth_manager=spotipy.SpotifyOAuth(
-                client_id=os.getenv("SPOTIPY_CLIENT_ID"),
-                client_secret=os.getenv("SPOTIPY_CLIENT_SECRET"),
-                redirect_uri=os.getenv("SPOTIPY_REDIRECT_URI"),
-                scope='playlist-read-private',
-                cache_path=os.path.join(".", ".cache")
-            )
+    sp = spotipy.Spotify(
+        auth_manager=spotipy.SpotifyOAuth(
+            client_id=os.getenv("SPOTIPY_CLIENT_ID"),
+            client_secret=os.getenv("SPOTIPY_CLIENT_SECRET"),
+            redirect_uri=os.getenv("SPOTIPY_REDIRECT_URI"),
+            scope='playlist-read-private',
+            cache_path=os.path.join(".", ".cache")
         )
-        printc("Connected to Spotify.", 'white', 0.2)
-        return sp
-    except spotipy.SpotifyException as e:
-        printc(f"Spotify exception: {e}", "red")
-        return None
-    except Exception as e:
-        printc(f"An unexpected error occurred: {e}", "red")
-        return None
+    )
+    printc("Connected to Spotify.", 'white', 0.2)
+    return sp
--- app/utils.py
+++ app/utils.py
@@ -6,6 +6,15 @@ from app.auth import os
 from PIL import Image
 from io import BytesIO
 
+def error_handler(func):
+    def wrapper(*args, **kwargs):
+        try:
+            result = func(*args, **kwargs)
+            return result
+        except Exception as e:
+            printc(f"Error in function {func.__name__}: {str(e)}", "red")
+    return wrapper
+
 def printc(message, color='white', duration=None):
     COLORS = {
         'reset': '\033[0m',
@@ -28,34 +37,26 @@ def printc(message, color='white', duration=None):
     if duration is not None:
         time.sleep(duration)
 
+@error_handler
 def to_json(data, file, mode):
-    try:
-        with open(file, mode, encoding='utf-8') as file:
-            json.dump(data, file, indent=2, ensure_ascii=False)
-            file.write("\n")
-    except Exception as e:
-        print(f"An error occurred while saving {data}: {e}")
+    with open(file, mode, encoding='utf-8') as file:
+        json.dump(data, file, indent=2, ensure_ascii=False)
+        file.write("\n")
 
+@error_handler
 def from_json(file):
-    try:
-        with open(file, 'r') as file:
-            config_data = json.load(file)
-            return config_data
-    except Exception as e:
-        print(f"Error loading {file}: {e}")
-        return None
-
+    with open(file, 'r') as file:
+        config_data = json.load(file)
+        return config_data
+    
+@error_handler
 def set_folder_icon(folder_path, image_url):
-    try:
-        response = get(image_url)
-        image_data = Image.open(BytesIO(response.content))
-        image_data.thumbnail((256, 256)) 
-        output_buffer = BytesIO()
-        image_data.save(output_buffer, format="PNG")
-        compressed_image_data = NSData.dataWithBytes_length_(output_buffer.getvalue(), len(output_buffer.getvalue()))
-        image = NSImage.alloc().initWithData_(compressed_image_data)
-        NSWorkspace.sharedWorkspace().setIcon_forFile_options_(image, folder_path, 0)
-
-        printc(f'Album image for "{os.path.basename(os.path.normpath(folder_path))}" folder defined.', "white")
-    except Exception as e:
-        printc(f"An error occurred while setting the icon for the folder '{folder_path}': {e}", "red")
+    response = get(image_url)
+    image_data = Image.open(BytesIO(response.content))
+    image_data.thumbnail((256, 256)) 
+    output_buffer = BytesIO()
+    image_data.save(output_buffer, format="PNG")
+    compressed_image_data = NSData.dataWithBytes_length_(output_buffer.getvalue(), len(output_buffer.getvalue()))
+    image = NSImage.alloc().initWithData_(compressed_image_data)
+    NSWorkspace.sharedWorkspace().setIcon_forFile_options_(image, folder_path, 0)
+    printc(f'Album image for "{os.path.basename(os.path.normpath(folder_path))}" folder defined.', "white")
